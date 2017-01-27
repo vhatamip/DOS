63e603a444e5bea11a8c800d71d407a0f8423ab3
Vahid <v.hpd84@gmail.com>
2017-01-26 17:24:51 -0700

    Basu's paper

---
+++ DOS/main.f90
@@ -0,0 +1,324 @@
+﻿!  LDOS.f90 
+!
+!  FUNCTIONS:
+!  LDOS - Local Density of State.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: LDOS
+!
+!  PURPOSE:  To calculate energy density U(ω,T) and LDOS(ω,T) at a distance
+!	"Δ" from medium zero (Sic) into medium 1 (vacuum).
+!
+!****************************************************************************
+    PROGRAM LDOS
+    
+	use mesh
+	!use properties
+	!use integral
+	
+	IMPLICIT NONE
+!****************************************************************************
+!
+!  Important numerical notes
+!  convg_crit=1E10-7 can not handel the resonance omega, convg_crit=1E10-8 fixed the problem
+!  
+!****************************************************************************    
+	INTEGER :: status
+	INTEGER :: i, j, l, m
+	INTEGER, PARAMETER :: omega_n_1=200, omega_n_2=1, omega_n_3=200
+    INTEGER, PARAMETER :: omega_n=omega_n_1+omega_n_2+omega_n_3-2
+	INTEGER, PARAMETER :: delta_n=3     !define number of deltas 
+	INTEGER, PARAMETER :: nodes=3 !N points of (k_ρ) from 0 to k_vac,
+										!simpson 1/3, ""N must be even"", k_vac is excluded from the first integral as k_z1=0.0.
+	INTEGER, PARAMETER :: n_k_rho_mesh=20
+	INTEGER, PARAMETER :: media_n=3
+	
+	REAL(KIND=8) :: error
+	REAL(KIND=8) :: d_k_rho, d_k_rho_temp, intg_e_t_old, intg_m_t_old
+	REAL(KIND=8), DIMENSION(delta_n) :: delta
+	REAL(KIND=8), DIMENSION(4) :: omega_range
+	REAL(KIND=8) :: int_1_min, int_2_min, int_3_min, int_1_max, int_2_max, int_3_max !omega values (intervals)
+	REAL(KIND=8), DIMENSION(omega_n_1) :: omega_1
+    REAL(KIND=8), DIMENSION(omega_n_2) :: omega_2
+    REAL(KIND=8), DIMENSION(omega_n_3) :: omega_3
+	REAL(KIND=8), DIMENSION(omega_n) :: omega
+	REAL(KIND=8), DIMENSION(nodes) :: k_rho
+	REAL(KIND=8), DIMENSION(nodes) :: f_k_rho_e, f_k_rho_m
+	REAL(KIND=8), ALLOCATABLE, DIMENSION(:,:) :: intg_e, intg_e_t, intg_m, intg_m_t, iter3, LDOSem
+	REAL(KIND=8), PARAMETER :: c0=299792458 !Speed of light in vacuum (m/s)
+	REAL(KIND=8), PARAMETER :: pi=4*ATAN(1.0) !pi=3.14...
+	!REAL(KIND=8), PARAMETER :: distance = 1.0E-8 ! 10nm
+	REAL(KIND=8), PARAMETER :: distance = 1.0 ! 1m
+	REAL(KIND=8), PARAMETER :: convg_crit=1.0E-9 ! convergence criterion 
+	                                             
+	COMPLEX(KIND=8), DIMENSION(media_n, omega_n) ::  eps, mu
+	COMPLEX(KIND=8), DIMENSION(media_n) ::  k
+	COMPLEX(KIND=8), DIMENSION(media_n,nodes) ::  k_z													
+	COMPLEX(KIND=8), DIMENSION(2,nodes) ::  r_TE, r_TM ! 1-->01 & 2-->02 (1:2,l) r01, r02 
+	COMPLEX(KIND=8), DIMENSION(2,nodes) ::  t_TE, t_TM ! 1 for t1 & 2 for t2																
+	
+    CHARACTER(LEN=20) :: filename
+!*****************************Initializing***********************************************
+	
+	omega=0.0; 
+	k_rho=0.0;
+	f_k_rho_e=0.0;
+	f_k_rho_m=0.0;
+	r_TE=0.0;
+	r_TM=0.0;
+	k_z=0.0	
+	
+!									Body of LDOS
+!************************************Data Import*****************************	   	   
+       OPEN ( UNIT=9, FILE='data.dat', STATUS='OLD', ACTION='READ', & 
+              &IOSTAT=status )
+	   ! Was the OPEN successful? 
+       fileopen: IF ( status == 0 ) THEN 
+       READ(9,90)
+       READ(9,90)
+       READ (9, 100, IOSTAT=status) (delta(i), i=1,delta_n)
+	   !PRINT*, 'delta', delta
+	   READ(9,90)
+       READ(9,90)
+	   !READ(9,90)
+       !READ(9,90)
+	   READ (9, 100, IOSTAT=status) (omega_range(i), i=1,4)
+	   !PRINT*, 'omega range', omega_range
+	   90  FORMAT(A80)
+       100 FORMAT(30X, E20.15)
+       ELSE fileopen 
+       ! Else file open failed. Tell user. 
+       WRITE (*,1030) status  
+       1030 FORMAT (1X,'data.dat open failed--status =', I6)
+	   END IF fileopen 
+!*******************************Omega discretization*********************************************
+int_1_min=omega_range(1)
+int_1_max=omega_range(2)
+	
+int_2_min=omega_range(2)
+int_2_max=omega_range(3)
+	
+int_3_min=omega_range(3)
+int_3_max=omega_range(4)
+	
+CALL cluster_mesh_right(int_1_min, int_1_max, omega_n_1, omega_1)
+!print*, omega_1
+CALL cluster_mesh_sides(int_2_min, int_2_max, omega_n_2, omega_2)
+!print*, omega_2
+CALL cluster_mesh_left(int_3_min, int_3_max, omega_n_3, omega_3)
+!print*, omega_3
+CALL sort_mesh(omega_1, omega_2, omega_3, omega)
+	  
+
+delta=(/0.01*distance, 0.6*distance, distance/)
+delta=(/1.0E-8, .6E-8, 5.0E-7/)
+
+PRINT*, 'delta', delta
+print*, distance
+print*, omega
+
+ALLOCATE(LDOSem(delta_n,omega_n), intg_e(delta_n,omega_n), intg_e_t(delta_n,omega_n), intg_m(delta_n,omega_n), intg_m_t(delta_n,omega_n), iter3(delta_n,omega_n))
+!***************************************before loop************************************************
+            LDOSem=0.0 
+            intg_e_t = 0.0
+			intg_m_t = 0.0
+			intg_e=0.0
+			intg_m=0.0
+			iter3=0.0
+
+       !DO i=1, delta_n !delta loop
+	   !DO i=2,2 !delta loop
+	    DO j=1, omega_n !omega loop
+			CALL mu_non_mag(j, omega(j), mu(:,j)) !ok
+			CALL eps_Basu(j, omega(j), eps(:,j)) !ok 
+			!CALL eps_Lorentz(j, omega(j), eps(:,j))
+		END DO	
+		OPEN(UNIT=26, FILE='eps_mu.dat', STATUS='unknown', ACTION='write', IOSTAT=status)
+		DO j=1,omega_n
+			WRITE(26,190)	omega(j), (eps(i,j), i=1,media_n), (mu(i,j), i=1, media_n)
+	    END DO
+	  !WRITE(27,190)	   
+!150	FORMAT (14X, '*****  RESULTS FILE FOR PROGRAM LDOS  *****',/,'******************************************************************')	   
+!160	FORMAT (T41, 'del1', T69, 'del2', T97, 'del3', T128, 'delta 1', T159, 'delta 2', T190, 'delta 3', T219,'delt1', T248,'delt2', T277,'delt3')
+!165	FORMAT (T13,'omega', T41, 'LDOS_e', T69, 'LDOS_e', T97, 'LDOS_e', T128, 'LDOS_m', T159, 'LDOS_m', T190, 'LDOS_m', T219,'iter', T248,'iter', T277,'iter')
+!170	FORMAT (15X, 9(E29.2))
+!175	FORMAT ('******************************************************************')
+!180	FORMAT (E25.15, 6(E30.20), 3(F10.1), 9(E30.20))	   
+190	FORMAT (E25.15, 6(2E30.20))
+		
+		
+		
+		
+		
+			
+	   !DO i=1, delta_n !delta loop
+	   DO i=2,2 !delta loop
+	    DO j=1, omega_n !omega loop
+		   	
+			!CALL mu_non_mag(j, omega(j), mu(:,j)) !ok
+			!CALL eps_Basu(j, omega(j), eps(:,j)) !ok
+			CALL k_mag(j, omega(j), mu(:,j), eps(:,j), k) !ok
+			
+			d_k_rho_temp=(k(1)-0.0)/(REAL(n_k_rho_mesh)-1) ! dk_ρ in the integral
+			
+			d_k_rho=d_k_rho_temp*(1+(0.5)/(REAL(n_k_rho_mesh)-2)) !for eliminating k_0
+			DO l=1,nodes !k_ρ in the first integral
+	        k_rho(l)=0.0+(l-1)*d_k_rho
+			END DO
+			
+			CALL wave_vector_z(j, mu(:,j), eps(:,j), k, k_rho, k_z)
+			CALL Fresnel(j, mu(:,j), eps(:,j), k_z, r_TM, r_TE)
+			CALL t_12(r_TM, r_TE, delta(i), distance, k_z, k_rho, t_TE, t_TM) !t_1(1)-->TE, t_1(2)-->TM
+            CALL integrand_k_rho_e(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_e)
+			CALL integrand_k_rho_m(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_m)
+!***********************************************intg1***********************************************
+			!PRINT*, 'n_k_rho_1', n_k_rho_1
+			!PRINT*, 'n_k_rho_2', n_k_rho_2
+			
+	   
+	        
+			
+			!coeff_int=1./(4*(pi**2)*omega(j)) !all the terms which are not a function of k_rho
+						
+			!CALL integrand_prop_k_rho(k_rho_1, mu_0, mu_1, eps_r0, eps_r1, k_vac, f_k_rho_1)	
+			
+			CALL intg_simp (nodes, d_k_rho, f_k_rho_e, intg_e(i,j))	
+			CALL intg_simp (nodes, d_k_rho, f_k_rho_m, intg_m(i,j))	
+			   		
+			intg_e_t(i,j) = intg_e_t(i,j) + intg_e(i,j)
+			intg_m_t(i,j) = intg_m_t(i,j) + intg_m(i,j)
+			intg_m_t_old = intg_m_t(i,j)
+			intg_e_t_old = intg_e_t(i,j)
+!***********************************************intg2***********************************************
+            !d_k_rho_2=(6*k_vac-k_vac)/(n_k_rho_2-1) ! dk_ρ in the integral
+	        !d_k_rho_2=d_k_rho_1  ! dk_ρ in the integral
+	        !DO k=1,n_k_rho_2 !k_ρ in the second integral from k_ρ to 12k_ρ
+	        !k_rho_2(k)=k_vac+(k-1)*d_k_rho_2
+			!END DO
+			
+			!coeff_int2=2.*coeff_int !all the terms which are not a function of k_rho
+						
+			!CALL integrand_evan_k_rho(k_rho_2,delta(i), mu_0, mu_1, eps_r0, eps_r1, k_vac, f_k_rho_2)	
+			!f_k_rho_2_temp(1:n_k_rho_2-1) = f_k_rho_2(2:n_k_rho_2)
+			!CALL intg_simp (n_k_rho_2-1, d_k_rho_2, f_k_rho_2_temp, intg2(i,j))
+				
+			     !intg2(i,j) = coeff_int2*intg2(i,j)					
+						
+!***********************************************int 3***********************************************		
+			
+			
+			
+			!d_k_rho=1*d_k_rho_temp	!to make finer k_rho mesh
+	        !k_rho_3=0.0; k_z0_3=0.0; k_z1_3=0.0; r_TM_10_3=0.0; f_k_rho_3=0.0; r_TE_10_3=0.0
+			
+	        !DO k=1,n_k_rho_3 !k_ρ in the third integral from 6k_ρ to inf
+	        !k_rho_3(k)=k_rho_intg2*k_vac+(k-1)*d_k_rho_3 !
+			!END DO
+			
+			!coeff_int3=coeff_int2 !all the terms which are not a function of k_rho
+						
+			!CALL integrand_evan_k_rho(k_rho_3,delta(i), mu_0, mu_1, eps_r0, eps_r1, k_vac, f_k_rho_3)
+							    
+			    !CALL intg_simp (n_k_rho_3, d_k_rho_3, f_k_rho_3, intg3(i,j))
+			    !intg3(i,j) = coeff_int3*intg3(i,j)
+				
+				!error=DABS((intg3(i,j)-intg3_old)/(intg1(i,j)+intg2(i,j)+intg3(i,j)))
+				!error=DABS(intg3(i,j))
+				!intg3_old=intg3(i,j)
+				
+		  iter3(i,j)=1								
+		  DO !iterative integral
+						  
+				iter3(i,j)=iter3(i,j)+1		!Counter
+				
+				DO l=1,nodes !k_ρ in the first integral
+	                 k_rho(l)=k_rho(nodes)+(l-1)*d_k_rho
+			    END DO
+			    !k_rho(1)=k_rho(3)
+				!k_rho(2)=k_rho(1)+d_k_rho
+				!k_rho(3)=k_rho(2)+d_k_rho
+				
+				CALL wave_vector_z(j, mu(:,j), eps(:,j), k, k_rho, k_z)
+			    CALL Fresnel(j, mu(:,j), eps(:,j), k_z, r_TM, r_TE)
+			    CALL t_12(r_TM, r_TE, delta(i), distance, k_z, k_rho, t_TE, t_TM) !t_1(1)-->TE, t_1(2)-->TM
+                CALL integrand_k_rho_e(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_e)
+			    CALL integrand_k_rho_m(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_m)
+				!CALL wave_vector_z(j, mu(:,j), eps(:,j), k, k_rho, k_z)
+			    !CALL Fresnel(j, mu(:,j), eps(:,j), k_z, r_TM, r_TE)
+			    !CALL t_12(r_TM, r_TE, delta(i), distance, k_z, k_rho, t_TE, t_TM) !t_1(1)-->TE, t_1(2)-->TM
+                !CALL integrand_k_rho_e(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_e)
+			    !CALL integrand_k_rho_m(omega(j), eps(2,j), k, k_rho, k_z, t_TE, t_TM, f_k_rho_m)
+			    
+				CALL intg_simp (nodes, d_k_rho, f_k_rho_e, intg_e(i,j))	
+			    CALL intg_simp (nodes, d_k_rho, f_k_rho_m, intg_m(i,j))	
+			   		
+			    intg_e_t(i,j) = intg_e_t(i,j) + intg_e(i,j)
+			    intg_m_t(i,j) = intg_m_t(i,j) + intg_m(i,j)
+				!intg3(i,j) = intg3(i,j) + intg3_iter(i,j)	
+				
+				!error=ABS((intg3(i,j)-intg3_old)/(intg3(i,j)))
+				error=MAX(DABS((intg_m_t(i,j)-intg_m_t_old)/(intg_m_t(i,j))), DABS((intg_e_t(i,j)-intg_e_t_old)/(intg_e_t(i,j))))
+				intg_m_t_old = intg_m_t(i,j)
+			    intg_e_t_old = intg_e_t(i,j)
+				
+				!IF (MOD(iter3(i,j),1000.0)==0) THEN
+				!print*, 'error', error, 'LDOS', intg_m_t(i,j)+intg_e_t(i,j), 'intg_m', intg_m_t(i,j), 'intg_e', intg_e_t(i,j), 'i', i, 'j', j, 'iter', iter3(i,j), 'k_rho', k_rho(nodes)
+				!END IF
+				
+				IF (error < convg_crit) EXIT
+				 				
+				
+		  END DO ! End  of iterative integral loop
+				
+			!intg(i,j)=intg1(i,j) +intg2(i,j) + intg3(i,j)	
+		    LDOSem(i,j)=intg_m_t(i,j)+intg_e_t(i,j)
+					
+			print*, 'error', error, 'LDOS', LDOSem(i,j), 'intg_m', intg_m_t(i,j), 'intg_e', intg_e_t(i,j), 'i', i, 'j', j, 'iter', iter3(i,j), 'k_rho', k_rho(nodes)
+			
+		END DO ! omega loop
+	   END DO  ! delta loop
+	   
+	   
+	   !DO i= 1,delta_n	!delta
+       ! Do j= 1,omega_n !omega
+           
+          !theta(j)=(h_plank*omega(j))/(DEXP((h_plank*omega(j))/(k_B*T))-1)  ! [J]
+          !U_omega(i,j)=theta(j)*intg(i,j)
+
+        !END DO
+       !END DO
+	   
+	   
+	   !*****************************************
+	   !intg_T=TRANSPOSE(intg)
+	   OPEN(UNIT=27, FILE='results.dat', STATUS='unknown', ACTION='write', IOSTAT=status)
+	   WRITE(27,150) 
+	   WRITE(27,160)
+       WRITE(27,170) (delta(i), i=1,delta_n), (delta(i), i=1,delta_n), (delta(i), i=1,delta_n)
+	   WRITE(27,165)
+	   !WRITE(27,175)
+	   
+	   
+	   
+		   DO j=1,omega_n
+			   
+			WRITE(27,180)	omega(j), (LDOSem(i,j), i=1,delta_n), (intg_e_t(i,j), i=1,delta_n), (intg_m_t(i,j), i=1,delta_n)
+			!WRITE(27,180)	omega(j), intg(2,j), intg1(2,j), intg2(2,j), intg3(2,j), iter3(2,j), U_omega(2,j) , (iter3(i,j), i=1,delta_n)   
+			   
+		   END DO
+	  !WRITE(27,190)	   
+150	FORMAT (14X, '*****  RESULTS FILE FOR PROGRAM LDOS  *****',/,'******************************************************************')	   
+160	FORMAT (T41, 'del1', T69, 'del2', T97, 'del3', T128, 'delta 1', T159, 'delta 2', T190, 'delta 3', T219,'delt1', T248,'delt2', T277,'delt3')
+165	FORMAT (T13,'omega', T41, 'LDOS_e', T69, 'LDOS_e', T97, 'LDOS_e', T128, 'LDOS_m', T159, 'LDOS_m', T190, 'LDOS_m', T219,'iter', T248,'iter', T277,'iter')
+170	FORMAT (15X, 9(E29.2))
+!175	FORMAT ('******************************************************************')
+!180	FORMAT (E25.15, 6(E30.20), 3(F10.1), 9(E30.20))	   
+180	FORMAT (E25.15, 9(E30.20))	
+!190 FORMAT ('******************************************************************',/, 6X, '*****  END OF THE RESULTS FILE FOR PROGRAM LDOS  *****')	  
+	
+	
+	
+	END PROGRAM LDOS   
+	   
---
+++ DOS/modoul.f90
@@ -0,0 +1,445 @@
+!  MeshGen.f90 
+!
+!  FUNCTIONS:
+!  MeshGen - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: MeshGen
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    
+MODULE mesh
+
+IMPLICIT NONE
+
+
+CONTAINS
+!*********************************************************************
+SUBROUTINE cluster_mesh_left(min_v,max_v,nodes,meshed)
+
+IMPLICIT NONE
+REAL(KIND=8), INTENT(IN) :: min_v, max_v
+INTEGER, INTENT(IN) :: nodes
+REAL(KIND=8), DIMENSION(nodes), INTENT(OUT) :: meshed
+
+!local
+REAL(KIND=8) :: L ! is the length of the interval
+REAL(KIND=8), PARAMETER :: b=1.0001 !closer to the value "one" the more clustered the mesh gets
+REAL(KIND=8), DIMENSION(nodes) :: k,a, temp
+INTEGER :: i
+
+L=max_v-min_v
+
+DO i=1,nodes
+    k(i)=0+(i-1)*(1/(REAL(nodes)-1))
+	a(i)=((b+1)/(b-1))**(1-k(i))
+    temp(i)=L*((b+1)-(b-1)*a(i))/(1+a(i))
+	meshed(i)=temp(i) + min_v
+END DO
+
+END SUBROUTINE cluster_mesh_left
+
+!*********************************************************************
+
+SUBROUTINE cluster_mesh_right(min_v,max_v,nodes,meshed)
+
+IMPLICIT NONE
+REAL(KIND=8), INTENT(IN) :: min_v, max_v
+INTEGER, INTENT(IN) :: nodes
+REAL(KIND=8), DIMENSION(nodes), INTENT(OUT) :: meshed
+
+!local
+REAL(KIND=8) :: L
+REAL(KIND=8), PARAMETER :: b=1.0001, alph=0.0
+REAL(KIND=8), DIMENSION(nodes) :: k, a, temp
+INTEGER :: i
+
+L=max_v-min_v
+
+DO i=1,nodes
+    k(i)=((i-1)/(REAL(nodes)-1)) ! 0 to 1 interval is uniformly meshed!
+	a(i)=((b+1)/(b-1))**((k(i)-alph)/(1-alph));
+    temp(i)=L*((b+2*alph)*a(i)-(b-2*alph))/((2*alph+1)*(1+a(i)))
+	meshed(i)=temp(i) + min_v
+END DO
+
+END SUBROUTINE cluster_mesh_right
+
+!*********************************************************************
+
+SUBROUTINE cluster_mesh_sides(min_v,max_v,nodes,meshed)
+
+IMPLICIT NONE
+REAL(KIND=8), INTENT(IN) :: min_v, max_v
+INTEGER, INTENT(IN) :: nodes
+REAL(KIND=8), DIMENSION(nodes), INTENT(OUT) :: meshed
+
+!local
+REAL(KIND=8) :: L
+REAL(KIND=8), PARAMETER :: b=1.0001, alpha=0.5
+REAL(KIND=8), DIMENSION(nodes) :: k, a, temp
+INTEGER :: i
+
+L=max_v-min_v
+
+DO i=1,nodes
+    k(i)=0+(i-1)*(1/(REAL(nodes)-1))
+	a(i)=((b+1)/(b-1))**((k(i)-alpha)/(1-alpha))
+    temp(i)=L*((b+2*alpha)*a(i)-(b-2*alpha))/((2*alpha+1)*(1+a(i)))
+	meshed(i)=temp(i) + min_v
+END DO
+
+END SUBROUTINE cluster_mesh_sides
+
+!*********************************************************************
+
+SUBROUTINE cluster_mesh_mid(min_v,max_v,nodes,meshed)
+
+IMPLICIT NONE
+REAL(KIND=8), INTENT(IN) :: min_v, max_v
+INTEGER, INTENT(IN) :: nodes
+REAL(KIND=8), DIMENSION(nodes), INTENT(OUT) :: meshed
+
+!local
+REAL(KIND=8) :: L, D
+REAL(KIND=8), PARAMETER :: b=15.0
+REAL(KIND=8), DIMENSION(nodes) :: k, a, temp
+INTEGER :: i
+
+L=max_v-min_v
+D=0.5*L ! clustered mesh in the middle of interval
+
+DO i=1,nodes
+    k(i)=0+(i-1)*(1/(REAL(nodes)-1))
+	a(i)=(0.5/b)*DLOG((1+(EXP(b)-1)*(D/L))/(1+(EXP(-b)-1)*(D/L)));
+    temp(i)=D*(1+(DSINH(b*(k(i)-a(i)))/DSINH(b*a(i))));
+    meshed(i)=temp(i) + min_v
+END DO
+
+END SUBROUTINE cluster_mesh_mid
+
+!*********************************************************************
+SUBROUTINE sort_mesh(meshed1, meshed2, meshed3, sorted) !right, sides, left
+
+IMPLICIT NONE
+!nodes_total=nodes_p1+nodes_p2+
+REAL(KIND=8), DIMENSION(:), INTENT(IN) :: meshed1, meshed2, meshed3
+REAL(KIND=8), DIMENSION(:), INTENT(OUT) :: sorted
+
+!local
+!INTEGER, DIMENSION(1) :: n1, n2, n3, n
+INTEGER :: n1, n2, n3, n, i
+REAL(KIND=8), ALLOCATABLE, DIMENSION(:) :: temp
+
+n1=UBOUND(meshed1,1) ! number of nodes in the first part 
+n2=UBOUND(meshed2,1) ! number of nodes in the middle part
+n3=UBOUND(meshed3,1) ! number of nodes in the last part
+
+n=n1+n2+n3-2
+ALLOCATE(temp(n)) 
+
+DO i=1,n1
+	temp(i)=meshed1(i)
+END DO
+DO i=1,n2-1
+	temp(n1+i)=meshed2(i+1)
+END DO
+DO i=1,n3-1
+	temp(n1+n2-1+i)=meshed3(i+1)
+END DO
+
+!ALLOCATE(sorted(n))
+sorted=temp
+
+END SUBROUTINE sort_mesh
+
+!****************************************************************************************
+!****************************************************************************************
+
+SUBROUTINE Fresnel(j, mu, eps, k_z, r_TM, r_TE)
+
+IMPLICIT NONE
+
+INTEGER, INTENT(IN) :: j
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: k_z
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(OUT) :: r_TM, r_TE
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: mu, eps
+
+INTEGER :: l, n
+
+n=UBOUND(k_z,2)
+                
+DO l=1, n
+            r_TM(1,l)=(eps(1) * k_z(2,l) - eps(2)*k_z(1,l))/(eps(1) * k_z(2,l) + eps(2) * k_z(1,l))
+	        r_TE(1,l)=(mu(1)  * k_z(2,l) - mu(2) *k_z(1,l))/(mu(1)  * k_z(2,l) + mu(2)  * k_z(1,l))
+			r_TM(2,l)=(eps(1) * k_z(3,l) - eps(3)*k_z(1,l))/(eps(1) * k_z(3,l) + eps(3) * k_z(1,l))
+	        r_TE(2,l)=(mu(1)  * k_z(3,l) - mu(3) *k_z(1,l))/(mu(1)  * k_z(3,l) + mu(3)  * k_z(1,l))
+END DO
+
+END SUBROUTINE Fresnel
+
+SUBROUTINE t_12(r_TM, r_TE, delta, distance, k_z, k_rho, t_TE, t_TM) !t_1(1)-->TE, t_1(2)-->TM
+
+REAL(KIND=8), INTENT(IN) :: delta, distance
+REAL(KIND=8), DIMENSION(:), INTENT(IN) :: k_rho
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: k_z
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: r_TM, r_TE
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(OUT) :: t_TE, t_TM !1-->t1, 2-->t2
+
+INTEGER :: l, n
+
+n=UBOUND(k_rho, 1)
+
+DO l=1, n
+	      t_TE(1,l)=(((1.0-r_TE(1,l))*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*delta))/(1-r_TE(1,l)*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))&
+		  -(((1.0-r_TE(1,l))*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*(distance-delta)))/(1-r_TE(1,l)*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))
+		  t_TE(2,l)=(((1.0-r_TE(1,l))*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*delta))/(1-r_TE(1,l)*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))&
+		  +(((1.0-r_TE(1,l))*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*(distance-delta)))/(1-r_TE(1,l)*r_TE(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))
+		  t_TM(1,l)=(((1.0-r_TM(1,l))*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*delta))/(1-r_TM(1,l)*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))&
+		  -(((1.0-r_TM(1,l))*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*(distance-delta)))/(1-r_TM(1,l)*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))
+		  t_TM(2,l)=(((1.0-r_TM(1,l))*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*delta))/(1-r_TM(1,l)*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))&
+		  +(((1.0-r_TM(1,l))*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*(distance-delta)))/(1-r_TM(1,l)*r_TM(2,l)*EXP(CMPLX(0.0,1.0)*2*k_z(1,l)*distance)))
+END DO
+	  
+END SUBROUTINE t_12
+
+
+SUBROUTINE wave_vector_z(j, mu, eps, k, k_rho, k_z) ! jth omega
+
+IMPLICIT NONE
+
+INTEGER, INTENT(IN) :: j
+REAL(KIND=8), DIMENSION(:), INTENT(IN) :: k_rho
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(OUT) :: k_z
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: k
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: mu, eps
+
+INTEGER :: l, q, n, m
+!COMPLEX(KIND=8), DIMENSION(:), ALLOCATABLE :: k_z0, k_z1, r_TM_10, r_TE_10
+
+n=UBOUND(k_rho,1) !number of nodes
+m=UBOUND(eps,1) !number of media
+!ALLOCATE(k_z(m,n))
+
+DO l=1, n
+	DO q=1,m
+			k_z(q,l)=SQRT(mu(q)*eps(q)*k(1)**2-k_rho(l)**2)
+	END DO		
+END DO
+
+END SUBROUTINE wave_vector_z
+
+SUBROUTINE k_mag(j, omega, mu, eps, k)
+
+IMPLICIT NONE
+
+INTEGER, INTENT(IN) :: j
+REAL(KIND=8), INTENT(IN) :: omega
+COMPLEX(KIND=8), DIMENSION(:), INTENT(OUT) :: k
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: mu, eps
+
+INTEGER :: l,n
+REAL(KIND=8), PARAMETER :: c0=299792458
+!COMPLEX(KIND=8), DIMENSION(:), ALLOCATABLE :: k_z0, k_z1, r_TM_10, r_TE_10
+n=UBOUND(eps,1)
+
+DO l=1, n
+        k(l)=SQRT(mu(l)*eps(l))*omega/c0    
+END DO
+
+
+END SUBROUTINE k_mag	
+
+
+SUBROUTINE eps_Basu(j, omega, eps) 
+
+IMPLICIT NONE
+
+INTEGER :: j
+REAL(KIND=8), INTENT(IN) :: omega
+COMPLEX(KIND=8), DIMENSION(:), INTENT(OUT) :: eps
+
+!INTEGER :: l, k
+REAL(KIND=8) :: eps_bl, n_e, e, m_0, m_star, eps_0, mu_1, mu_max, mu_2, c_r, c_s, alpha
+REAL(KIND=8) :: betha, T_1, T_2, mu_mob_300, omega_p, mu_mob_T_1, tau_1, mu_mob_T_2, tau_2     
+
+eps_bl=11.7;
+n_e=1E20*1E6; !(1/m**3)
+e=1.60217662E-19; ! Basu's paper "infrared..."
+m_0=9.109E-31;! Basu's paper "infrared..."
+m_star=0.27*m_0;
+eps_0=8.854187817E-12; ! Basu's paper "infrared..."
+mu_1=68.5;
+mu_max=1414;
+mu_2=56.1;
+c_r=9.2E16*1E6; !(1/m**3)
+c_s=3.41E20*1E6; !(1/m**3)
+alpha=0.711;
+betha=1.98;
+T_1=400; !Kelvin & emitter
+T_2=0.0; !Kelvin & reciever
+
+mu_mob_300=1E-4*(mu_1+((mu_max-mu_1)/(1+(n_e/c_r)**alpha))-(mu_2/(1+(c_s/n_e)**betha))); !m**2/(V.s)
+omega_p=SQRT((n_e*e**2)/(m_star*eps_0));
+
+mu_mob_T_1=mu_mob_300*(T_1/300)**1.5; !mob --> mobility
+tau_1=(m_star*mu_mob_T_1)/e;
+
+
+mu_mob_T_2=mu_mob_300*(T_2/300)**1.5; 
+tau_2=(m_star*mu_mob_T_2)/e;
+
+IF (tau_1==0.0) THEN 
+	eps(1)=DCMPLX(1.0,0.0)
+    eps(2)=eps_bl
+    eps(3)=eps_bl-(omega_p**2)/(omega*(omega+(DCMPLX(0.0,1.0)/tau_2)));
+ELSE IF (tau_2==0.0) THEN
+	eps(1)=DCMPLX(1.0,0.0)
+    eps(2)=eps_bl-((omega_p**2)/(omega*(omega+(DCMPLX(0.0,1.0)/tau_1))));
+    eps(3)=eps_bl
+END IF
+
+!eps(3)=eps_inf*(DCMPLX(omega(j)**2-omega_lo**2 , gamma*omega(j))/DCMPLX(omega(j)**2-omega_to**2 , gamma*omega(j)))
+
+END SUBROUTINE eps_Basu
+
+SUBROUTINE eps_Lorentz(j, omega, eps)
+
+IMPLICIT NONE
+
+INTEGER :: j
+REAL(KIND=8), INTENT(IN) :: omega
+COMPLEX(KIND=8), DIMENSION(:), INTENT(OUT) :: eps
+
+REAL(KIND=8), PARAMETER :: eps_inf=6.7, gamma=8.966E11	!Equation 4, 2009, ()(s^-1)
+REAL(KIND=8), PARAMETER :: omega_lo=1.825E14, omega_to=1.494E14  !Equation 4, 2009, (rad s^-1)(rad s^-1)
+
+eps(1)=DCMPLX(1.0,0.0)
+eps(2)=eps_inf*(DCMPLX(omega**2-omega_lo**2 , gamma*omega)/DCMPLX(omega**2-omega_to**2 , gamma*omega))
+eps(3)=eps(2)
+
+END SUBROUTINE eps_Lorentz
+
+SUBROUTINE mu_mag(omega, mu) ! Magnetic medium
+
+IMPLICIT NONE
+
+REAL(KIND=8), INTENT(IN) :: omega
+COMPLEX(KIND=8), DIMENSION(:), INTENT(OUT) :: mu
+
+mu(1)=DCMPLX(1.0,0.0)
+mu(2)=DCMPLX(1.0,0.0)
+mu(3)=DCMPLX(1.0,0.0)
+
+END SUBROUTINE mu_mag
+
+SUBROUTINE mu_non_mag(j, omega, mu) !Non-Magnetic medium
+
+IMPLICIT NONE
+
+INTEGER :: j
+REAL(KIND=8), INTENT(IN) :: omega
+COMPLEX(KIND=8), DIMENSION(:), INTENT(OUT) :: mu
+
+mu(1)=DCMPLX(1.0,0.0)
+mu(2)=DCMPLX(1.0,0.0)
+mu(3)=DCMPLX(1.0,0.0)
+
+END SUBROUTINE mu_non_mag
+
+!****************************************************************************************
+!****************************************************************************************
+
+SUBROUTINE integrand_k_rho_e(omega, eps_2, k, k_rho, k_z, t_TE, t_TM, f_k_rho_e)
+
+REAL(KIND=8), INTENT(IN) :: omega
+REAL(KIND=8), DIMENSION(:), INTENT(IN) :: k_rho
+REAL(KIND=8), DIMENSION(:), INTENT(OUT) :: f_k_rho_e
+COMPLEX(KIND=8), INTENT(IN) :: eps_2
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: k
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: k_z
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: t_TE, t_TM
+
+INTEGER :: l, n
+REAL(KIND=8), PARAMETER :: c0=299792458
+REAL(KIND=8), PARAMETER :: pi=4*ATAN(1.0) !pi=3.14...
+
+n=UBOUND(k_rho,1)
+
+DO l=1, n
+	      f_k_rho_e(l)=(((ABS(k_z(1,l)*t_TM(1,l)))**2+(ABS(k_rho(l)*t_TM(2,l)))**2)*(((k_rho(l))**2+k_z(2,l)*CONJG(k_z(2,l)))&
+		  /(k(2)*CONJG(k(2))))+(ABS(k(1)*t_TE(2,l)))**2)*((k_rho(l))/(((ABS(k_z(2,l)))**2)*DIMAG(k_z(2,l))))
+END DO
+
+f_k_rho_e=((omega*DIMAG(eps_2))/(16*(pi**2)*c0**2))*f_k_rho_e
+
+END SUBROUTINE integrand_k_rho_e
+
+SUBROUTINE integrand_k_rho_m(omega, eps_2, k, k_rho, k_z, t_TE, t_TM, f_k_rho_m)
+
+REAL(KIND=8), INTENT(IN) :: omega
+REAL(KIND=8), DIMENSION(:), INTENT(IN) :: k_rho
+REAL(KIND=8), DIMENSION(:), INTENT(OUT) :: f_k_rho_m
+COMPLEX(KIND=8), INTENT(IN) :: eps_2
+COMPLEX(KIND=8), DIMENSION(:), INTENT(IN) :: k
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: k_z
+COMPLEX(KIND=8), DIMENSION(:,:), INTENT(IN) :: t_TE, t_TM
+
+INTEGER :: l, n
+REAL(KIND=8), PARAMETER :: c0=299792458
+REAL(KIND=8), PARAMETER :: pi=4*ATAN(1.0) !pi=3.14...
+
+n=UBOUND(k_rho,1)
+
+DO l=1, n
+	      f_k_rho_m(l)=(((ABS(k(1)*t_TM(2,l)))**2)*(((k_rho(l))**2+k_z(2,l)*CONJG(k_z(2,l)))/(k(2)*CONJG(k(2))))+(ABS(k_z(3,l)&
+		  *t_TE(1,l)))**2+(ABS(k_rho(l)*t_TE(2,l)))**2)*((k_rho(l))/(((ABS(k_z(2,l)))**2)*DIMAG(k_z(2,l))))
+END DO
+
+f_k_rho_m=((omega*DIMAG(eps_2))/(16*(pi**2)*c0**2))*f_k_rho_m
+
+END SUBROUTINE integrand_k_rho_m
+
+
+SUBROUTINE intg_simp (n, h, f, intg)
+!
+! Subroutine for integration over f(x) with the Simpson rule. f:
+! integrand f(x); h: interval(dx); intg: integral.
+IMPLICIT NONE
+INTEGER, INTENT (IN) :: n
+INTEGER :: i
+REAL(KIND=8), INTENT (IN) :: h
+REAL(KIND=8) :: s0,s1,s2
+REAL(KIND=8), INTENT (OUT) :: intg
+REAL(KIND=8), INTENT (IN), DIMENSION (:) :: f
+!
+
+intg = 0.0
+s0 = 0.0
+s1 = 0.0
+s2 = 0.0
+DO i = 2, n-1, 2
+s1 = s1+f(i-1)
+s0 = s0+f(i)
+s2 = s2+f(i+1)
+END DO
+intg = h*(s1+4.0*s0+s2)/3.0
+!
+! If n is even, add the last slice separately
+!
+IF (MOD(n,2).EQ.0) intg = intg &
++h*(5.0*f(n)+8.0*f(n-1)-f(n-2))/12.0
+END SUBROUTINE intg_simp
+	
+!****************************************************************************************
+!****************************************************************************************
+
+
+END MODULE mesh
+
+
